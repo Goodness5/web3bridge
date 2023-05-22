4980c8c9f2c1f937666a43c31682485915a450d8
goodness5 <goodnesskolapo@gmail.com>
2023-05-22 13:39:20 +0100

    okay

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import requests
 import networkx as nx
 import plotly.graph_objects as go
 from flask import Flask, render_template
-from datetime import datetime
+import time
 
 app = Flask(__name__)
 
@@ -30,31 +30,30 @@ def index():
         header = next(csv_reader)  # Skip header row
 
         for row in csv_reader:
-            if len(row) >= 3:  # Check if row has at least 2 elements
+            if len(row) >= 3:  # Check if row has at least 3 elements
                 email = row[1]
                 username = row[0]
                 github_repos = get_user_repos(username)  # Fetch repository data
 
                 for repo in github_repos:
-                    created_date = repo["created_at"]
-                    created_date = datetime.strptime(created_date, "%Y-%m-%dT%H:%M:%SZ")
-                    if created_date >= datetime(2021, 6, 1):
-                        repo["email"] = email
-                        repo["username"] = username
-                        repos_data.append(repo)
+                    repo["email"] = email
+                    repo["username"] = username
+                    repos_data.append(repo)
+                    time.sleep(1)  # Pause execution for 1 second between API requests
 
     # Create a network graph using NetworkX
     G = nx.Graph()
 
     for repo in repos_data:
-        user = repo["owner"]["login"]
+        user = repo["username"]
         repo_name = repo["name"]
         repo_url = repo["html_url"]
-        G.add_node(repo_name, user=user, email=repo["email"], url=repo_url)
+        G.add_node(user, type="user")
+        G.add_node(repo_name, type="repo", user=user, email=repo["email"], url=repo_url)
         G.add_edge(user, repo_name)
 
-    # Create a layout for the network graph using fruchterman_reingold_layout
-    pos = nx.fruchterman_reingold_layout(G)
+    # Create a layout for the network graph using random_geometric_graph
+    layout = nx.random_geometric_layout(G)
 
     # Extract node and edge data for visualization
     node_x = []
@@ -64,15 +63,18 @@ def index():
     edge_x = []
     edge_y = []
 
-    for node, (x, y) in pos.items():
-        node_x.append(x)
-        node_y.append(y)
+    for node, coords in layout.items():
+        node_x.append(coords[0])
+        node_y.append(coords[1])
         node_text.append(node)
-        node_hovertext.append(f"User: {G.nodes[node]['user']}<br>Repository: {node}")
+        if G.nodes[node]["type"] == "user":
+            node_hovertext.append(f"User: {node}")
+        else:
+            node_hovertext.append(f"User: {G.nodes[node]['user']}<br>Repository: {node}")
 
     for edge in G.edges:
-        x0, y0 = pos[edge[0]]
-        x1, y1 = pos[edge[1]]
+        x0, y0 = layout[edge[0]]
+        x1, y1 = layout[edge[1]]
         edge_x.append(x0)
         edge_x.append(x1)
         edge_x.append(None)
@@ -101,19 +103,15 @@ def index():
     data = [edge_trace, node_trace]
 
     layout = go.Layout(
-        title="GitHub Repository Network",
+        title="GitHub User-Repository Network",
         showlegend=False,
         hovermode="closest",
         margin=dict(b=20, l=5, r=5, t=40),
         xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
-        yaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
-        plot_bgcolor="black",
-        paper_bgcolor="black"
+        yaxis=dict(showgrid=False, zeroline=False, showticklabels=False)
     )
 
     fig = go.Figure(data=data, layout=layout)
-
-    # Convert the plot to HTML
     plot_html = fig.to_html(full_html=False)
 
     return render_template('index.html', plot_html=plot_html)
